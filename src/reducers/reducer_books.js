c20aeb7ac319d86e7b7eafad4710b40f8a4821eb
Sacha <sachamorgese@gmail.com>
2017-10-29 15:36:13 +0900

    Chore: Added several files, app basic functions working

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
     return [
-        {title: 'Javascript: The Good Parts'},
-        {title: 'Harry Potter'},
-        {title: 'The Dark Tower'},
-        {title: 'Eloquent Ruby'},
+        {title: 'Javascript: The Good Parts', pages: 5},
+        {title: 'Harry Potter', pages: 'a shitload'},
+        {title: 'The Dark Tower', pages: 'thousands of lots of thousands'},
+        {title: 'Eloquent Ruby', pages: 555},
     ]
 }
